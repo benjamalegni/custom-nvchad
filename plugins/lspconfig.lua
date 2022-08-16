b87d3e097e1c348ba00226b2df653b0ac9a0c100
benjamalegni <lukabenjaminmalegni@gmail.com>
2022-08-16 16:27:45 -0300

    lspserver update

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -5,7 +5,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd","tsserver","java_language_server","kotlin_language_server","svelte","rust_analyzer", "emmet_ls","pyright","vuels"}
+local servers = { "html", "cssls", "clangd","tsserver","java_language_server","kotlin_language_server","svelte","rust_analyzer", "emmet_ls","pyright","vuels","jsonls"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
